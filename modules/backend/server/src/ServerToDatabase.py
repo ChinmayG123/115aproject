3dbe754df05093b97d0dd33a0b6d1da9f421e865
mdong5-ucsc <mdong5@ucsc.edu>
2024-04-11 18:24:01 -0700

    create template database access functions

--- modules/backend/server/src/ServerToDatabase.py
+++ modules/backend/server/src/ServerToDatabase.py
@@ -9,8 +9,35 @@ class DatabaseAccess:
 
         - param1 username: The username as a string.
         - param2 password: The password as a string.
-        - return: 0 for success, other values for failure.
+        - return: 0 for success, 1 for user not exits, 2 for incorrect password, -1 for internal failure.
         """
         # Here, you would add your MongoDB access code
         return 0
 
+    def add_new_user(self, username, password):
+        """
+        - param1 username: The username as a string.
+        - param2 password: The password as a string.
+        - return: 0 for success, 1 for user already exits, 2 for invalid password format, -1 for internal failure.
+        """
+        return 0;
+
+    def update_user_status(self, username, status):
+        """
+        This function update the database about whether a user is online. 
+
+        - param1 username: The username as a string.
+        - param2 status: The log in status (online or offline) as a boolean.
+        - return: 0 for success, -1 for internal failure which will force client to logout.
+        """
+        return 0
+    
+    def retrieve_user_data(self, username):
+        """
+        This function looks up for user's game progress (learned words, defeated boss, unlocked language, etc.). 
+
+        - param1 username: The username as a string.
+        - return: a string of user data for success, empty string for internal failure which will force client to logout.
+        """
+        data = "somedata"
+        return data
