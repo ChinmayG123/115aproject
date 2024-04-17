1955ee4a297273399e91cc0b747ed5af29681218
mdong5-ucsc <mdong5@ucsc.edu>
2024-04-17 13:42:55 -0700

    archived credential

--- modules/backend/server/VocabServer.py
+++ modules/backend/server/VocabServer.py
@@ -1,5 +1,53 @@
 #!/usr/bin/env python3
+
+# pre-condition validations
+# check python packages
+import subprocess
+import pkg_resources
+import sys
 import os
+import shutil
+
+script_path = os.path.abspath(__file__)
+server_dir_path = os.path.dirname(script_path)
+database_dir_path = os.path.dirname(server_dir_path) + "/database"
+
+
+def install_package(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+def check_and_install(package):
+    try:
+        pkg_resources.get_distribution(package)
+    except pkg_resources.DistributionNotFound:
+        print(f"{package} not found, installing...")
+        install_package(package)
+    else:
+        print(f"{package} is already installed")
+
+
+packages = ["pyzipper", "firebase", "firebase-admin"]
+for package in packages:
+    check_and_install(package)
+
+# decrypt credentials
+import pyzipper
+
+
+def extract_protected_zip(zip_filename, password, extract_to="."):
+    """Extract a password-protected ZIP file."""
+    with pyzipper.AESZipFile(zip_filename, "r") as zf:
+        zf.setpassword(password.encode("utf-8"))
+        zf.extractall(path=extract_to)
+
+
+zip_file = database_dir_path + "/" + "credential.zip"
+password = "arichinmaychristophermingweivenkatesh"
+extract_protected_zip(zip_file, password, database_dir_path + "/cfg")
+print("Credential decrypted.")
+
+
 from datetime import datetime
 import socket
 import threading
@@ -11,12 +59,29 @@ server_socket = None
 termination = False
 debug_mode = True
 
+
 def signal_handler(signum, frame):
     global termination
     print("Signal received, shutting down server.")
     termination = True
     if server_socket:
         server_socket.close()
+        
+    delete_files_in_directory(database_dir_path + "/cfg")
+    print("Credential deleted.")
+
+def delete_files_in_directory(directory):
+    """Delete all files in the specified directory without removing the directory itself."""
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print("Failed to delete %s. Reason: %s" % (file_path, e))
+
 
 def worker(client_socket, address):
     thread_id = threading.current_thread().ident
@@ -26,12 +91,13 @@ def worker(client_socket, address):
     if debug_mode:
         print(datetime.now(), f" - Thread: {thread_id} exit")
 
+
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
     signal.signal(signal.SIGQUIT, signal_handler)
     signal.signal(signal.SIGTSTP, signal_handler)
-    
+
     HOST = ""
     PORT = 8080
 
@@ -39,13 +105,18 @@ if __name__ == "__main__":
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((HOST, PORT))
     server_socket.listen()
-    print(datetime.now(), f" - Server listening on {socket.gethostbyname(socket.gethostname())}:{PORT}")
+    print(
+        datetime.now(),
+        f" - Server listening on {socket.gethostbyname(socket.gethostname())}:{PORT}",
+    )
 
     while not termination:
         try:
             client_socket, address = server_socket.accept()
             print(datetime.now(), f" - Accepted connection from {address}")
-            client_thread = threading.Thread(target=worker, args=(client_socket, address))
+            client_thread = threading.Thread(
+                target=worker, args=(client_socket, address)
+            )
             client_thread.start()
         except OSError:
             break  # Break from the loop if server_socket is closed by signal handler
