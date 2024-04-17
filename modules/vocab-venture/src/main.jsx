4acbece859486c851d8f041a0486ed31f9c4ba1b
Ari Ferreros <tinytabby66@gmail.com>
2024-04-17 13:39:13 -0700

    removed bootstrap, working navigation to different pages, not implemented with js function yet

--- modules/vocab-venture/src/main.jsx
+++ modules/vocab-venture/src/main.jsx
@@ -1,14 +1,27 @@
 //main javascript file 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-//import 'bootstrap/dist/css/bootstrap.css'
-//import './index.css'
-import './scss/main.scss'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import MainPage from "./pages/MainPage/MainPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+
+export default function App() {
+  
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route exact path= "/home" element={<MainPage></MainPage>}/>
+        <Route index element= {<MainPage></MainPage>}/>
+        <Route exact path= "/login" element={<LoginPage></LoginPage>}/>
+        <Route exact path= "/register" element={<RegisterPage></RegisterPage>}/>
+
+        </Routes>
+    </BrowserRouter>
     
-    <App /> {/*app component that we imported*/}
-  </React.StrictMode>,
-)
+  );  
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
