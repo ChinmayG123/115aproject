26d564fa73939db7b33148ab8ce6cef44a0d0d08
ChinmayG123 <__chinmay.gowdru@ushur.com>
2024-04-18 19:27:34 -0700

    frontend

--- modules/vocab-venture/src/pages/MapPage/MapPage.jsx
+++ modules/vocab-venture/src/pages/MapPage/MapPage.jsx
@@ -1,8 +1,28 @@
 import React, { useEffect } from 'react'; // Import useEffect from react
 import { useNavigate } from 'react-router-dom/dist';
+import background from '../../assets/MenuBG.png';
 
-export default function Map() {
-return(
-    <p>hi</p>
-);
+import './MapPage.css';
+
+// export default function Map() {
+// return(
+//     <p>hi</p>
+// );
+// };
+
+
+
+const Map = function() {
+
+    return(  
+        <body className = "mapbackground">
+            {/* <div className='mapbg'> */}
+                <h1>Vocab Venture</h1>
+                {/* <img src= {background} id= "backgroundpic" className= "center"/> */}
+            {/* </div> */}
+        </body>
+ 
+     );
 };
+
+export default Map;
