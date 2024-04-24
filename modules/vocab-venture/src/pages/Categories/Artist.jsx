d7edf9954309261d2b6036d6d8d30abb58d35b5b
Ari Ferreros <tinytabby66@gmail.com>
2024-04-23 18:31:13 -0700

    artist npc page ui

--- modules/vocab-venture/src/pages/Categories/Artist.jsx
+++ modules/vocab-venture/src/pages/Categories/Artist.jsx
@@ -1,12 +1,25 @@
 import { useNavigate } from 'react-router-dom/dist';
+import npcimg from '../../assets/artist-assets/Artist.png';
+import learnBG from '../../assets/artist-assets/Contentbox.png';
+import npcTextbox from '../../assets/artist-assets/ArtistTextbox.png';
+import easel from '../../assets/artist-assets/Easel.png';
+import artistbg from '../../assets/artist-assets/ArtistBG.png';
+import './Artist.css';
 
 const Artist = function() {
 
     return(  
-        <body className = "mapbackground">
-            <h1>Artist</h1>
-
-        </body>
+        <div className = "container">
+            <img id= "artistbg" src={artistbg}></img>
+            <div className = "learn-content">
+                <img id= "learnBG" src={learnBG} />
+            </div>
+            <div className = "npc-content">
+                <img id= "npcTextbox" src={npcTextbox} />
+                <img id = "npcimg" src={npcimg} />
+            </div>
+            <img id= "easelimg" src={easel}></img>
+        </div>
  
      );
 };
