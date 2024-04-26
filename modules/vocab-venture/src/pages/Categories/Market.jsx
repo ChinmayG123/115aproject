df89e6673794147a4e161ce15e325648d73ab4ad
Ari Ferreros <tinytabby66@gmail.com>
2024-04-26 13:36:03 -0700

    Market UI implemented

--- modules/vocab-venture/src/pages/Categories/Market.jsx
+++ modules/vocab-venture/src/pages/Categories/Market.jsx
@@ -1,13 +1,63 @@
-import { useNavigate } from 'react-router-dom/dist';
+// import { useNavigate } from 'react-router-dom/dist';
+// import { useNavigate } from 'react-router-dom';
+
+import npcimg from '../../assets/market-assets/ShopOwner.png';
+import learnBG from '../../assets/market-assets/Contentbox.png';
+import npcTextbox from '../../assets/market-assets/MarketTextbox.png';
+import stall from '../../assets/market-assets/Stall.png';
+import marketbg from '../../assets/market-assets/MarketBG.png';
+import './Artist.css';
+import './Market.css';
+
+// import { useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+
+import React, { useState } from 'react';
+
 
 const Market = function() {
 
-    return(  
-        <body className = "mapbackground">
-            <h1>Market</h1>
+    const navigate = useNavigate(); 
 
-        </body>
+    const goToMenu =() => {navigate('/home')};
+
+
+    return(  
+
+        <div className = "container">
+            
+              
+            <img id= "artistbg" src={marketbg}></img>
+            <div className = "learn-content">
+                <img id= "learnBG" src={learnBG} />
+            </div>
+            <div className = "npc-content">
+                {/* <img id= "npcTextbox" src={npcTextbox} />
+                <img id = "npcimg" src={npcimg} /> */}
  
+                {/* {showText && <p>This is the NPC's text.</p>} */}
+            
+                <img id="marketnpcimg" src={npcimg} alt="npc image" />
+                <img id= "easelimg" src={stall}></img>
+                <img id="npcTextbox" src={npcTextbox} alt="npc text box" />
+                
+
+                {/* {showText && <p>This is the NPC's text.</p>} */}
+            </div>
+            
+            <div className= "textdiv">
+                <input type="text" className = "learnInputBox" placeholder='text'/>
+            </div>
+
+            {/* <button onClick={goToMenu}>Go to Menu</button> */}
+            {/* <button type= "button" id = "nextbutton" onClick= {goToMenu}>Next</button> */}
+            {/* <button type="button" id="nextbutton" onClick={showNpcText}>Next</button> */}
+
+
+                  
+        </div>
+    
      );
 };
 
