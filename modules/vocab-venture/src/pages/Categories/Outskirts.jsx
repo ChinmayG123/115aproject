0f7ac94cdcb11f727870448e8af51a77600cdb47
verangarajan <verangar@ucsc.edu>
2024-05-09 17:38:11 -0700

    added MC button

--- modules/vocab-venture/src/pages/Categories/Outskirts.jsx
+++ modules/vocab-venture/src/pages/Categories/Outskirts.jsx
@@ -1,10 +1,55 @@
-import { useNavigate } from 'react-router-dom/dist';
+
+
+// import { useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+import React, { useState, useEffect } from 'react';
+
+
 
 const Outskirts = function() {
+
+
+    const navigate = useNavigate(); 
+
+
+    const location = useLocation();
+    const username = location.state.username;
+    const selectedlanguage = location.state.language;
+
+
+
+    const goToType = () => {
+        // navigate('/map', { state: { username } });
+        console.log("usernameeeee", username);
+        console.log("languageeeee", selectedlanguage);
+        navigate('/type', { state: { username, language: selectedlanguage } });
+    };
+
+
+    const multipleChoice = () => {
+        // navigate('/map', { state: { username } });
+        console.log("user", username);
+        console.log("language", selectedlanguage);
+        navigate('/type', { state: { username, language: selectedlanguage } });
+    };
+
 
     return(  
         <body className = "mapbackground">
             <h1>Outskirts</h1>
+
+
+            <button type="button" id="goToTypeButton" onClick={goToType}>
+                Go to Type
+            </button>
+
+            <button type="button" id="MultipleChoiceButton" onClick={multipleChoice}>
+                MultipleChoice
+            </button>
+
+
+                
 
         </body>
  
