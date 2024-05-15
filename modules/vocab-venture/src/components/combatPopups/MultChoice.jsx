b4a454ae38f036305d2d0d2a4ff146174e3c758e
ChinmayG123 <__chinmay.gowdru@ushur.com>
2024-05-14 18:18:49 -0700

    change

--- modules/vocab-venture/src/components/combatPopups/MultChoice.jsx
+++ modules/vocab-venture/src/components/combatPopups/MultChoice.jsx
@@ -6,7 +6,8 @@ import learnBG from '../../assets/artist-assets/Contentbox.png';
 import npcTextbox from '../../assets/artist-assets/ArtistTextbox.png';
 import easel from '../../assets/artist-assets/Easel.png';
 import artistbg from '../../assets/artist-assets/ArtistBG.png';
-// import './MultChoice.css';
+import './MultChoice.css';
+
 
 
 
@@ -17,6 +18,64 @@ import React, { useState, useEffect } from 'react';
 // import React, { useState } from 'react';
 
 
+import blue from '../../assets/dict-images/colors/blue.png';
+import black from '../../assets/dict-images/colors/black.png';
+import brown from '../../assets/dict-images/colors/brown.png';
+import green from '../../assets/dict-images/colors/green.png';
+import orange from '../../assets/dict-images/colors/orange.png';
+import red from '../../assets/dict-images/colors/red.png';
+import white from '../../assets/dict-images/colors/white.png';
+import yellow from '../../assets/dict-images/colors/yellow.png';
+
+import apple from '../../assets/dict-images/food/Apple.png';
+import bread from '../../assets/dict-images/food/Bread.png';
+import egg from '../../assets/dict-images/food/Eggs.png';
+import fish from '../../assets/dict-images/food/Fish.png';
+import juice from '../../assets/dict-images/food/Juice.png';
+import meat from '../../assets/dict-images/food/Meat.png';
+import milk from '../../assets/dict-images/food/Milk.png';
+import water from '../../assets/dict-images/food/Water.png';
+
+
+const getWordImageSrc = (wordImage) => {
+    switch (wordImage) {
+        case 'black':
+            return black;
+        case 'blue':
+            return blue;
+        case 'brown':
+            return brown;
+        case 'red':
+            return red;
+        case 'yellow':
+            return yellow;
+        case 'green':
+            return green;
+        case 'white':
+            return white;
+        case 'orange':
+            return orange;
+        case 'apple':
+          return apple;
+        case 'bread':
+            return bread;
+        case 'egg':
+            return egg;
+        case 'fish':
+            return fish;
+        case 'juice':
+            return juice;
+        case 'meat':
+            return meat;
+        case 'milk':
+            return milk;
+        case 'water':
+            return water;
+        default:
+            return null;
+    }
+  };
+  
 
 const Type = function() {
 
@@ -227,13 +286,6 @@ const Type = function() {
 
         <div className = "container">
             
-            
-        {correctMessage && (
-            <div className="message-display">
-                <p>{correctMessage}</p>
-            </div>
-        )}
-
 
 
             <div className = "learned-words1" >
@@ -242,15 +294,28 @@ const Type = function() {
                         // englishword = key;
                         if (index === currentWordIndex)
                         return (
+                    
                           <div key={key} className="word-container">
+
+                            <div className="image-container">
+                                {getWordImageSrc(key) && (
+                                  <img
+                                    id="wordImage1"
+                                    src={getWordImageSrc(key)}
+                                    alt={key}
+                                  />
+                                )}
+                              </div>
                               <div className="word-info">
-                                <h1 style={{ marginTop: '250px', marginLeft: '200px' }} >English: {key}</h1>
+                                <h1 style={{ marginTop: '300px', marginLeft: '550px' }} >English: {key}</h1>
                                 </div>
                         </div>
                         );
                       return null;
                     })}
 
+
+            
 
                 <div className="word-buttons">
                     {wordChoices.map((spanishWord, index) => (
@@ -259,6 +324,14 @@ const Type = function() {
                         </button>
                     ))}
                 </div>
+
+
+                {correctMessage && (
+                            <div className="message-display">
+                                <p>{correctMessage}</p>
+                            </div>
+                        )}
+
 
               </div>
 
